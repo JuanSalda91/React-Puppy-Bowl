0ff986ab5d13cc4f998afe9e65de858b63c8be40
JuanSalda91 <jpsalda6@hotmail.com>
2025-02-03 00:49:03 -0500

    Puppy bowl React

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import AllPlayers from "./components/AllPlayers.jsx";
+import SinglePlayer from "./components/SinglePlayer.jsx";
+import NewPlayerForm from "./components/NewPlayerForm.jsx";
+import NavBar from "./components/NavBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Puppy Selector</h1>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<AllPlayers />} />
+          <Route path="/players/:id" element={<SinglePlayer />} />
+          <Route path="/new-player" element={<NewPlayerForm />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
